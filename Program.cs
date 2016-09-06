d8fa5e302df9efdb3c57a99fec9d822addbf7277
Markus Himmel <markus@himmel-villmar.de>
2016-09-06 10:32:50 +0200

    Update to current version

--- Program.cs
+++ Program.cs
@@ -1,22 +1,34 @@
 ﻿/*
+ * TemplateCS
  * Copyright (C) 2014-2016 Markus Himmel
  * This file is distributed under the terms of the MIT license
  */
 
-#define CodeJam
-//#define CodeForces
+//#define CodeJam
+#define CodeForces
 
 using System;
-using System.Linq;
-using System.IO;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+
+// ReSharper disable RedundantUsingDirective
+using System.IO;
+
+// ReSharper restore RedundantUsingDirective
+
+// ReSharper disable UnusedMember.Local
+
+// ReSharper disable once CheckNamespace
 
 namespace ProgrammingCompetitions
 {
-	class Program
+	internal class Program
 	{
+		#region GCJ
 
-#region GCJ
 #if CodeJam
 		private static bool DEBUG = false;
 
@@ -32,26 +44,32 @@ namespace ProgrammingCompetitions
 			}
 		}
 #endif
-#endregion
 
-#region CodeForces
+		#endregion
+
+		#region CodeForces
+
 #if CodeForces
-#if !ONLINE_JUDGE
-		static Queue<string> testInput = new Queue<string>(new string[]
-		{
-			"",
-		});
+#if DEBUG
+		private static readonly Queue<string> testInput = new Queue<string>(@"
+
+1 1
+
+
+".Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries));
 #endif
 
-		static string Solve()
+		private static object solve()
 		{
+			// ReSharper disable once RedundantOverflowCheckingContext
 			checked
 			{
 				return null;
 			}
 		}
 #endif
-#endregion
+
+		#endregion
 
 		// Everything after this comment is template code
 
@@ -62,12 +80,13 @@ namespace ProgrammingCompetitions
 
 		private static T[] readMany<T>()
 		{
+			// ReSharper disable once PossiblyMistakenUseOfParamsMethod
 			return readMany<T>(' ');
 		}
 
-		private static _[] readMany<_>(params char[] ___)
+		private static T[] readMany<T>(params char[] ___)
 		{
-			return read().Split(___).Select(__ => (_)Convert.ChangeType(__, typeof(_))).ToArray();
+			return read().Split(___).Select(__ => (T)Convert.ChangeType(__, typeof(T))).ToArray();
 		}
 
 		private static string[] readMany()
@@ -77,21 +96,17 @@ namespace ProgrammingCompetitions
 
 		private static T[][] readMany<T>(int n)
 		{
-			T[][] res = new T[n][];
+			var res = new T[n][];
 			for (int i = 0; i < n; i++)
-			{
 				res[i] = readMany<T>();
-			}
 			return res;
 		}
 
 		private static T[][] readField<T>(int height, Func<char, T> map)
 		{
-			T[][] res = new T[height][];
+			var res = new T[height][];
 			for (int _ = 0; _ < height; _++)
-			{
-				res[_] = read().Select(c => map(c)).ToArray();
-			}
+				res[_] = read().Select(map).ToArray();
 			return res;
 		}
 
@@ -107,20 +122,20 @@ namespace ProgrammingCompetitions
 
 		private static void read<T1>(out T1 t1)
 		{
-			var vals = readMany();
+			string[] vals = readMany();
 			t1 = (T1)Convert.ChangeType(vals[0], typeof(T1));
 		}
 
 		private static void read<T1, T2>(out T1 t1, out T2 t2)
 		{
-			var vals = readMany();
+			string[] vals = readMany();
 			t1 = (T1)Convert.ChangeType(vals[0], typeof(T1));
 			t2 = (T2)Convert.ChangeType(vals[1], typeof(T2));
 		}
 
 		private static void read<T1, T2, T3>(out T1 t1, out T2 t2, out T3 t3)
 		{
-			var vals = readMany();
+			string[] vals = readMany();
 			t1 = (T1)Convert.ChangeType(vals[0], typeof(T1));
 			t2 = (T2)Convert.ChangeType(vals[1], typeof(T2));
 			t3 = (T3)Convert.ChangeType(vals[2], typeof(T3));
@@ -128,7 +143,7 @@ namespace ProgrammingCompetitions
 
 		private static void read<T1, T2, T3, T4>(out T1 t1, out T2 t2, out T3 t3, out T4 t4)
 		{
-			var vals = readMany();
+			string[] vals = readMany();
 			t1 = (T1)Convert.ChangeType(vals[0], typeof(T1));
 			t2 = (T2)Convert.ChangeType(vals[1], typeof(T2));
 			t3 = (T3)Convert.ChangeType(vals[2], typeof(T3));
@@ -137,7 +152,7 @@ namespace ProgrammingCompetitions
 
 		private static void read<T1, T2, T3, T4, T5>(out T1 t1, out T2 t2, out T3 t3, out T4 t4, out T5 t5)
 		{
-			var vals = readMany();
+			string[] vals = readMany();
 			t1 = (T1)Convert.ChangeType(vals[0], typeof(T1));
 			t2 = (T2)Convert.ChangeType(vals[1], typeof(T2));
 			t3 = (T3)Convert.ChangeType(vals[2], typeof(T3));
@@ -145,13 +160,13 @@ namespace ProgrammingCompetitions
 			t5 = (T5)Convert.ChangeType(vals[4], typeof(T5));
 		}
 
-		static Func<T1, TResult> DP<T1, TResult>(Func<T1, Func<T1, TResult>, TResult> f)
+		private static Func<T1, TResult> dp<T1, TResult>(Func<T1, Func<T1, TResult>, TResult> f)
 		{
 			var cache = new Dictionary<Tuple<T1>, TResult>();
 			Func<T1, TResult> res = null;
-			res = (t1) =>
+			res = t1 =>
 			{
-				var key = Tuple.Create(t1);
+				Tuple<T1> key = Tuple.Create(t1);
 				if (!cache.ContainsKey(key))
 					cache.Add(key, f(t1, res));
 				return cache[key];
@@ -159,13 +174,13 @@ namespace ProgrammingCompetitions
 			return res;
 		}
 
-		static Func<T1, T2, TResult> DP<T1, T2, TResult>(Func<T1, T2, Func<T1, T2, TResult>, TResult> f)
+		private static Func<T1, T2, TResult> dp<T1, T2, TResult>(Func<T1, T2, Func<T1, T2, TResult>, TResult> f)
 		{
 			var cache = new Dictionary<Tuple<T1, T2>, TResult>();
 			Func<T1, T2, TResult> res = null;
 			res = (t1, t2) =>
 			{
-				var key = Tuple.Create(t1, t2);
+				Tuple<T1, T2> key = Tuple.Create(t1, t2);
 				if (!cache.ContainsKey(key))
 					cache.Add(key, f(t1, t2, res));
 				return cache[key];
@@ -173,13 +188,13 @@ namespace ProgrammingCompetitions
 			return res;
 		}
 
-		static Func<T1, T2, T3, TResult> DP<T1, T2, T3, TResult>(Func<T1, T2, T3, Func<T1, T2, T3, TResult>, TResult> f)
+		private static Func<T1, T2, T3, TResult> dp<T1, T2, T3, TResult>(Func<T1, T2, T3, Func<T1, T2, T3, TResult>, TResult> f)
 		{
 			var cache = new Dictionary<Tuple<T1, T2, T3>, TResult>();
 			Func<T1, T2, T3, TResult> res = null;
 			res = (t1, t2, t3) =>
 			{
-				var key = Tuple.Create(t1, t2, t3);
+				Tuple<T1, T2, T3> key = Tuple.Create(t1, t2, t3);
 				if (!cache.ContainsKey(key))
 					cache.Add(key, f(t1, t2, t3, res));
 				return cache[key];
@@ -187,13 +202,13 @@ namespace ProgrammingCompetitions
 			return res;
 		}
 
-		static Func<T1, T2, T3, T4, TResult> DP<T1, T2, T3, T4, TResult>(Func<T1, T2, T3, T4, Func<T1, T2, T3, T4, TResult>, TResult> f)
+		private static Func<T1, T2, T3, T4, TResult> dp<T1, T2, T3, T4, TResult>(Func<T1, T2, T3, T4, Func<T1, T2, T3, T4, TResult>, TResult> f)
 		{
 			var cache = new Dictionary<Tuple<T1, T2, T3, T4>, TResult>();
 			Func<T1, T2, T3, T4, TResult> res = null;
 			res = (t1, t2, t3, t4) =>
 			{
-				var key = Tuple.Create(t1, t2, t3, t4);
+				Tuple<T1, T2, T3, T4> key = Tuple.Create(t1, t2, t3, t4);
 				if (!cache.ContainsKey(key))
 					cache.Add(key, f(t1, t2, t3, t4, res));
 				return cache[key];
@@ -201,58 +216,49 @@ namespace ProgrammingCompetitions
 			return res;
 		}
 
-		static IEnumerable<T> single<T>(T it)
+		private static IEnumerable<T> single<T>(T it)
 		{
 			yield return it;
 		}
 
-		static IEnumerable<long> range(long first, long last, long step = 1)
+		private static IEnumerable<long> range(long first, long last, long step = 1)
 		{
 			for (long i = first; i <= last; i += step)
-			{
 				yield return i;
-			}
 		}
 
-		static IEnumerable<int> range(int first, int last, int step = 1)
+		private static IEnumerable<int> range(int first, int last, int step = 1)
 		{
-			for (int i = first; i <= last; i+= step)
-			{
+			for (int i = first; i <= last; i += step)
 				yield return i;
-			}
 		}
 
-		static T id<T>(T a)
+		private static T id<T>(T a)
 		{
 			return a;
 		}
 
-		public static T[][] mkarr<T>(int x, int y)
+		public static T[][] Mkarr<T>(int x, int y)
 		{
-			T[][] res = new T[x][];
+			var res = new T[x][];
 			for (int i = 0; i < x; i++)
-			{
 				res[i] = new T[y];
-			}
 			return res;
 		}
 
-		public static T[][][] mkarr<T>(int x, int y, int z)
+		public static T[][][] Mkarr<T>(int x, int y, int z)
 		{
-			T[][][] res = new T[x][][];
+			var res = new T[x][][];
 			for (int i = 0; i < x; i++)
 			{
 				res[i] = new T[y][];
 				for (int j = 0; j < y; j++)
-				{
 					res[i][j] = new T[z];
-				}
 			}
 			return res;
 		}
 
-		static bool[] tf = new[] { true, false };
-
+		private static bool[] tf = { true, false };
 
 #if CodeJam
 
@@ -316,18 +322,32 @@ namespace ProgrammingCompetitions
 #endif
 
 #if CodeForces
-		static void Main(string[] args)
+		private static string asString(object s)
 		{
-#if !ONLINE_JUDGE
-			var sw = new System.Diagnostics.Stopwatch();
+			if (s is double)
+				return ((double)s).ToString("0.000000000", CultureInfo.InvariantCulture);
+			return s.ToString();
+		}
+
+		public static void Main(string[] args)
+		{
+#if DEBUG
+			Stopwatch sw = new Stopwatch();
 			sw.Start();
 #endif
-			string s = Solve();
-			if (!string.IsNullOrEmpty(s))
+			object o = solve();
+			if (o != null)
 			{
-				Console.WriteLine(s);
+				string s;
+				IEnumerable e = o as IEnumerable;
+				if ((e != null) && !(o is string))
+					s = string.Join(" ", e.OfType<object>().Select(asString));
+				else
+					s = asString(o);
+				if (!string.IsNullOrEmpty(s))
+					Console.WriteLine(s);
 			}
-#if !ONLINE_JUDGE
+#if DEBUG
 			sw.Stop();
 			Console.WriteLine(sw.Elapsed);
 			Console.ReadKey();
@@ -336,7 +356,7 @@ namespace ProgrammingCompetitions
 
 		private static string read()
 		{
-#if ONLINE_JUDGE
+#if !DEBUG
 			return Console.ReadLine();
 #else
 			return testInput.Dequeue();
@@ -347,6 +367,24 @@ namespace ProgrammingCompetitions
 
 	public static class ExtensionMethods
 	{
+		public static IEnumerable<T> Of<T>(this int n, Func<T> gen)
+		{
+			for (int i = 0; i < n; i++)
+				yield return gen();
+		}
+
+		public static IEnumerable<T> Of<T>(this int n, Func<long, T> gen)
+		{
+			for (int i = 0; i < n; i++)
+				yield return gen(i);
+		}
+
+		public static IEnumerable<T> Of<T>(this int n, T gen)
+		{
+			for (int i = 0; i < n; i++)
+				yield return gen;
+		}
+
 		public static IEnumerable<TOut> Cartesian<T1, T2, TOut>(this IEnumerable<T1> first, IEnumerable<T2> second, Func<T1, T2, TOut> collector)
 		{
 			return first.SelectMany(f => second.Select(s => collector(f, s)));
@@ -380,11 +418,10 @@ namespace ProgrammingCompetitions
 
 		public static IEnumerable<IEnumerable<T>> Cartesian<T>(this IEnumerable<IEnumerable<T>> source)
 		{
-			IEnumerable<IEnumerable<T>> res = source.First().Select(x => single(x));
-			foreach (IEnumerable<T> next in source.Skip(1))
-			{
+			IEnumerable<T>[] enumerable = source as IEnumerable<T>[] ?? source.ToArray();
+			IEnumerable<IEnumerable<T>> res = enumerable.First().Select(single);
+			foreach (IEnumerable<T> next in enumerable.Skip(1))
 				res = res.Cartesian(next, (sofar, n) => sofar.Concat(single(n)));
-			}
 			return res;
 		}
 
@@ -395,29 +432,23 @@ namespace ProgrammingCompetitions
 
 		public static IEnumerable<T> Demask<T>(this IEnumerable<Tuple<T, bool>> inp)
 		{
-			foreach (var pair in inp)
-			{
+			foreach (Tuple<T, bool> pair in inp)
 				if (pair.Item2)
-				{
 					yield return pair.Item1;
-				}
-			}
 		}
 
 		public static IEnumerable<IEnumerable<IEnumerable<T>>> Partition<T>(this IEnumerable<T> source, int groups)
 		{
-			foreach (var part in Enumerable.Range(0, groups).Pow(source.Count()).Select(x => x.ToArray()))
-			{
-				yield return Enumerable.Range(0, groups).Select(x => source.Where((item, idx) => part[idx] == x));
-			}
+			T[] src = source as T[] ?? source.ToArray();
+			foreach (int[] part in Enumerable.Range(0, groups).Pow(src.Length).Select(x => x.ToArray()))
+				yield return Enumerable.Range(0, groups).Select(x => src.Where((item, idx) => part[idx] == x));
 		}
 
 		public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T> it)
 		{
-			foreach (var conf in new[] { true, false }.Pow(it.Count()))
-			{
-				yield return it.Zip(conf, Tuple.Create).Demask();
-			}
+			T[] itt = it as T[] ?? it.ToArray();
+			foreach (IEnumerable<bool> conf in new[] { true, false }.Pow(itt.Length))
+				yield return itt.Zip(conf, Tuple.Create).Demask();
 		}
 
 		private static IEnumerable<T> single<T>(T it)
@@ -425,49 +456,41 @@ namespace ProgrammingCompetitions
 			yield return it;
 		}
 
-		private static IEnumerable<T> ExceptSingle<T>(this IEnumerable<T> first, T it, IEqualityComparer<T> comp = null)
+		private static IEnumerable<T> exceptSingle<T>(this IEnumerable<T> first, T it, IEqualityComparer<T> comp = null)
 		{
 			comp = comp ?? EqualityComparer<T>.Default;
 			bool seen = false;
 			foreach (T a in first)
-			{
 				if (!seen && comp.Equals(a, it))
-				{
 					seen = true;
-				}
 				else
-				{
 					yield return a;
-				}
-			}
 		}
 
 		public static IEnumerable<IEnumerable<T>> Permutations<T>(this IEnumerable<T> it)
 		{
-			if (it.Count() < 2)
+			T[] src = it as T[] ?? it.ToArray();
+			if (src.Length < 2)
 			{
-				yield return it;
+				yield return src;
 				yield break;
 			}
 
-			foreach (T first in it)
-			{
-				foreach (IEnumerable<T> part in Permutations(it.ExceptSingle(first)))
-				{
+			foreach (T first in src)
+				foreach (IEnumerable<T> part in Permutations(src.exceptSingle(first)))
 					yield return single(first).Concat(part);
-				}
-			}
 		}
 
 		public static T[][] Rho<T>(this IEnumerable<T> source, int x, int y)
 		{
-			T[][] res = Program.mkarr<T>(x, y);
+			T[][] res = Program.Mkarr<T>(x, y);
 
 			int i = 0, j = 0;
 
+			T[] src = source as T[] ?? source.ToArray();
 			while (true)
 			{
-				foreach (T item in source)
+				foreach (T item in src)
 				{
 					res[i][j] = item;
 					j++;
@@ -488,12 +511,13 @@ namespace ProgrammingCompetitions
 
 		public static T[][][] Rho<T>(this IEnumerable<T> source, int x, int y, int z)
 		{
-			T[][][] res = Program.mkarr<T>(x, y, z);
+			T[][][] res = Program.Mkarr<T>(x, y, z);
 
 			int i = 0, j = 0, k = 0;
+			T[] src = source as T[] ?? source.ToArray();
 			while (true)
 			{
-				foreach (T item in source)
+				foreach (T item in src)
 				{
 					res[i][j][k] = item;
 					k++;
@@ -506,9 +530,7 @@ namespace ProgrammingCompetitions
 							j = 0;
 							i++;
 							if (i == x)
-							{
 								break;
-							}
 						}
 					}
 				}
